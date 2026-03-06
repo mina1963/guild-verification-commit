778d6954ed49a30295cd55a99b04df8a81f09a85
mina1963 <minaprotocol1963@gmail.com>
2026-03-07 02:32:27 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T23:32:08.399Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-06T23:32:27.292Z
+Commit: Fix race condition in async operations
