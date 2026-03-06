41f8aeed114b7f2cd600a0e50c741ad99b80f5f2
mina1963 <minaprotocol1963@gmail.com>
2026-03-07 02:33:22 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T23:32:26.262Z
-Commit: Add multi-language support
+Timestamp: 2026-03-06T23:33:22.134Z
+Commit: Resolve null pointer exception in user service
