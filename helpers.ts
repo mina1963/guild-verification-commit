b993cc9d142667c8084ffc08d1c7e91c8c5caf85
mina1963 <minaprotocol1963@gmail.com>
2026-03-07 02:32:00 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T23:31:43.789Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-06T23:32:00.791Z
+Commit: Fix memory leak in event handlers
