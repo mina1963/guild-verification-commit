3595174496d485bec77b132fddfbfed8e769e3da
mina1963 <minaprotocol1963@gmail.com>
2026-03-07 02:32:34 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T23:32:27.292Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-06T23:32:34.249Z
+Commit: Fix memory leak in event handlers
